bd5d782ae22e66fa7d9cbdc2763b585f644babcb
Crystalis89 <131834837+Crystalis89@users.noreply.github.com>
2024-07-16 15:38:48 -0400

    Replaced list traverser while loop used for some of the functions with  a function that does same.
    The functions that need to find the tail or value use same while loop to traverse the linked list so separated it out into a function that they can share.
    
    The exceptions being:
    
     Pop because it checks the next next so can update the new tail before removing
    
     At because it iterates to the given index value thus using a For loop.
    
    toString because it is building the text output with each loop.
    
    listVisualizer same as toString.

--- scripts.js
+++ scripts.js
@@ -23,6 +23,7 @@ function listVisualizer(list) {
             }
 
     while (listdepth.next !== null) {
+        
 
         let listentry = document.createElement('li')
         if (listdepth.head === null && listdepth.next !== null) {
@@ -46,6 +47,29 @@ function listVisualizer(list) {
         listelement.appendChild(listentry)
     }
        
+}
+
+function listTraverser(list, value) {
+    let targetnode = list
+    let result = {}
+    let i = 0
+    while (targetnode.next !== null || i < 10000) {
+
+        if (targetnode.head === null && targetnode.next === null) {
+            return result = {'target': targetnode, 'depth': i}
+        }
+
+        if (targetnode.next === null && targetnode.data !== value && targetnode.head !== null) {
+            return result = {'target': targetnode, 'depth': i}
+        } 
+        
+        if(targetnode.data === value && targetnode.head !== null) {
+            return result = {'target': targetnode, 'depth': i, 'valuedepth': i}
+        }
+        i++;
+        targetnode = targetnode.next; 
+  
+    }
 }
 
 
@@ -74,28 +98,21 @@ class LinkedList {
 //Maybe add way to submit an array of list node entries for it to iterate through and add them all instead of calling the function multiple times
     append(list, value) {
         let createnode = LinkedList.createListNode(value)
-
-                  
+        let tailnode = listTraverser(list)
+            
         if (list.head === null) {
-            let listdepth = list   
-        
-            if (listdepth.head === null && listdepth.next === null) { 
-                listdepth.next = createnode
+
+            if (list.head === null && list.next === null) { 
+                tailnode.target.next = createnode
 
             } else {
-                let i = 0;
 
-                while (listdepth !== null || i < 10000) {
-                    i++;
-                    listdepth = listdepth.next;
-                   
-                    if (listdepth.next === null) {
-                        listdepth.next = createnode
-                        listVisualizer(list)
-                        return                    
-                    } 
 
-                }
+                if (tailnode.target.next === null) {
+                    tailnode.target.next = createnode
+                    listVisualizer(list)
+                    return                    
+                } 
             }
 
         } else {
@@ -120,21 +137,17 @@ class LinkedList {
 
     // size returns the total number of nodes in the list
     size(list) {
-        let i = 0;
-        let listdepth = list
 
-        while (listdepth !== null && i < 10000) {
-            i++;
-            listdepth = listdepth.next;
-           
-            if (listdepth.next === null) {
-                return i                    
-            } 
+        list = listTraverser(list)
+
+        if (list.target.next === null) {
+            return list.depth
+        } 
+      
+        if (list.target.head === null) {
+            return list.depth
         }
-        if (list.head === null)
-            return i
     }
-
 
        // head returns the first node in the list
     headNode(list) {
@@ -143,20 +156,13 @@ class LinkedList {
             return list
         }   
   
-
     // tail returns the last node in the list
     tailNode(list) {
-        let i = 0;
-        let listdepth = list
-
-        while (listdepth !== null || i < 10000) {
-            i++;
-            listdepth = listdepth.next;
-           
-            if (listdepth.next === null) {
-                return listdepth                                   
-            } 
-        }
+             
+        list = listTraverser(list)
+        if (list.target.next === null) {
+            return list.target                                   
+        } 
     }
 
     // at(index) returns the node at the given index
@@ -181,61 +187,51 @@ class LinkedList {
         let i = 0
         let listdepth = list
           
-        while (listdepth.next !== null || i < 10000) {
+        while (listdepth.next.next !== null || i < 10000) {
 
-            i++;
- 
             if (listdepth.next.next === null) {
-            
-                newvar.push(listdepth.next)
-                listdepth.next = null
-                listVisualizer(list)
-                return 
-                
+                break
+            } else {
+            i++;
+            listdepth = listdepth.next; 
             }
-            listdepth = listdepth.next
+         
+        }
 
+        if (listdepth.next.next === null) {
+            
+            newvar.push(listdepth.next)
+            listdepth.next = null
+            listVisualizer(list)
+            return 
+            
         }
 
     }
 
     // contains(value) returns true if the passed in value is in the list and otherwise returns false.
     contains(list, value){
+        list = listTraverser(list, value)
+     
+        if (list.target.data === value) {
+            return true                                  
+        } 
 
-        let i = 0
-        let listdepth = list
-   
-
-        while (listdepth.next !== null || i < 10000) {
-            i++;
-            listdepth = listdepth.next
-           
-            if (listdepth.data === value) {
-                return true                                  
-            } 
-
-            if (listdepth.next === null) {
-                return false
-            }
+        if (list.target.next === null) {
+            return false
         }
 
     }
 
     // find(value) returns the index of the node containing value, or null if not found.
     find(list, value){
+        list = listTraverser(list, value)
 
-        let i = 0
-        let listdepth = list
-   
-
-        while (listdepth.next !== null || i < 10000) {
-            i++;
-            listdepth = listdepth.next
-           
-            if (listdepth.data === value) {
-                return i                                  
-            } 
-        }
+        if (list.valuedepth) {
+               return list.valuedepth
+        } else {
+            return false
+        }       
 
     }
 
@@ -245,7 +241,6 @@ class LinkedList {
         let i = 0
         let listdepth = list.next
    
-
         while (listdepth !== null || i < 10000) {
             i++;
             nodestring += ` ${listdepth.data},`
@@ -281,6 +276,6 @@ let headnode = LinkedList.createHead()
 
 
 headnode.append(headnode, '1 append test')
-headnode.append(headnode, '2 appened test')
+headnode.append(headnode, '2 append test')
 headnode.append(headnode, '3 append test')
 headnode.prepend(headnode, '1 prepend test')
